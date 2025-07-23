cf7f87384c7a677a92739d4136c8b4c26d21482e
ariaandika <andikaakun17@gmail.com>
2025-07-23 13:46:57 +0700

    add find_lt

--- find-nul-term.rs
+++ find-nul-term.rs
@@ -13,37 +13,66 @@ macro_rules! logb {
 }
 
 fn main() {
-    let value = b"Si\0md Sw";
+    let value = b"Simd Sw0";
+    assert!(find_nul(value).is_none());
 
+    let value = b"Simd\0 Sw";
     let i = find_nul(value).unwrap();
-
     assert_eq!(value[i], b'\0');
+
+    let value = b"Simd Sw\0";
+    let i = find_nul(value).unwrap();
+    assert_eq!(value[i], b'\0');
+
+    let value = b"\0Simd Sw";
+    let i = find_nul(value).unwrap();
+    assert_eq!(value[i], b'\0');
+
+    //
+
+    let value = [124, 125, 126, 220, 127, 128, 129, 121];
+    assert!(find_lt(value, 100).is_none());
+
+    let value = [124, 125, 126, 20, 127, 128, 129, 11];
+    let i = find_lt(value, 100).unwrap();
+    assert!(value[i] < 100);
+
+    let value = [124, 125, 126, 133, 127, 128, 129, 20];
+    let i = find_lt(value, 100).unwrap();
+    assert!(value[i] < 100);
+
+    let value = [24, 125, 126, 133, 127, 128, 129, 130];
+    let i = find_lt(value, 100).unwrap();
+    assert!(value[i] < 100);
 }
 
-fn find_nul(mut bytes: &[u8]) -> Option<usize> {
-    loop {
-        if let Some((chunk, rest)) = bytes.split_first_chunk() {
-            if let Some(ok) = find_nul_chunk(chunk) {
-                return Some(ok);
-            }
-            bytes = rest;
-        } else {
-            return bytes.iter().position(|e| e == &0);
-        }
-    }
-}
-
-fn find_nul_chunk(chunk: &[u8; CHUNK_SIZE]) -> Option<usize> {
+fn find_nul(chunk: &[u8; CHUNK_SIZE]) -> Option<usize> {
     let x = usize::from_ne_bytes(*chunk);
 
     let x2 = x | x << 1;
     let x4 = x2 | x2 << 2;
     let x8 = x4 | x4 << 4;
-    let byte_map = !x8 & 0x8080808080808080;
+    let found = !x8 & 0x8080808080808080;
 
-    if byte_map == 0 {
+    if found == 0 {
         None
     } else {
-        Some((byte_map.trailing_zeros() / 8) as usize)
+        Some((found.trailing_zeros() / 8) as usize)
+    }
+}
+
+/// Find the first byte that less than
+fn find_lt(chunk: [u8; CHUNK_SIZE], byte: u8) -> Option<usize> {
+    const MSB: usize = usize::from_ne_bytes([128; CHUNK_SIZE]);
+
+    let bm = usize::from_ne_bytes([byte; CHUNK_SIZE]);
+    let chunk = usize::from_ne_bytes(chunk);
+    let eq_b = chunk.wrapping_sub(bm) & !chunk;
+    let found = eq_b & MSB;
+
+    if found == 0 {
+        None
+    } else {
+        Some((found.trailing_zeros() / 8) as usize)
     }
 }
