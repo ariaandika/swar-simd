8aa6ae51fdec520074efce6a10e612f8d89b11f0
ariaandika <andikaakun17@gmail.com>
2025-07-27 21:36:06 +0700

    add find_sse implementation

--- src/lib.rs
+++ src/lib.rs
@@ -82,6 +82,49 @@ pub unsafe fn find_raw(start: *const u8, end: *const u8, byte: u8) -> Option<usi
     None
 }
 
+
+/// Find byte in a bytes.
+#[inline]
+pub fn find_sse(value: &[u8], byte: u8) -> Option<usize> {
+    unsafe { find_sse_raw(value.as_ptr(), value.as_ptr().add(value.len()), byte) }
+}
+
+#[target_feature(enable = "sse4.2")]
+fn find_sse_raw(start: *const u8, end: *const u8, byte: u8) -> Option<usize> {
+    const CHUNK_SSE_SIZE: usize = size_of::<usize>();
+    const CHUNK_SSE_ISIZE: isize = size_of::<usize>() as isize;
+
+    use std::arch::x86_64::*;
+
+    let target = _mm_set1_epi8(byte as i8);
+    let mut current = start;
+
+    unsafe {
+        while end.offset_from(current) > CHUNK_SSE_ISIZE {
+            let chunk = _mm_loadu_si128(current as _);
+
+            let cmp = _mm_cmpeq_epi8(chunk, target);
+
+            let mask = _mm_movemask_epi8(cmp) as u32;
+
+            if mask != 0 {
+                // Find the first set bit (index of the match)
+                // let pos = mask.trailing_zeros() as usize;
+                // return Some(offset + pos);
+
+                let pos = mask.trailing_zeros() as usize;
+                let offset = current.offset_from(start) as usize;
+
+                return Some(offset.unchecked_add(pos));
+            }
+
+            current = current.add(CHUNK_SSE_SIZE);
+        }
+    }
+
+    None
+}
+
 /// Find the first either 2 byte in a bytes.
 #[inline]
 pub fn find2(value: &[u8], b1: u8, b2: u8) -> Option<usize> {
